37f390e6064aa603c3e5db2455e10d58a99df245
Caleb Luster <cwl117@outlook.com>
2022-02-22 17:56:15 -0600

    got it kinda sort of working

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -2,40 +2,102 @@
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if (license !== "None") {
-    return `![GitHub license](https://img.shields.io/badge/license-${license}-red.svg)`
+    return `![GitHub license](https://img.shields.io/badge/license-${license}-red.svg)`;
   }
-  return ""
+  return "";
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
   if (license !== "None") {
-    return (
-      `\n* [License](#license)\n`
-    )
+    return `\n* [License](#license)\n`;
   }
-  return ""
+  return "";
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
   if (license !== "None") {
-    return (
-      `### License 
+    return `### License 
       Licensed under ${license}.
       Copyright ${license}. All rights reserved.
-      `
-    )
+      `;
   }
-  return ""
+  return "";
 }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.projectTitle}
 
+
+    ${renderLicenseBadge(data.license)}
+
+
+    # GitHub Link
+
+
+    [${data.githubUsername}](https://github.com/${data.githubUsername}/)
+
+
+    # Project Description
+
+
+    ${data.projectDescription}
+
+
+    # Table of Contents
+
+
+    * [Uses](#usage)
+
+
+    * [Installations](#dependencies)
+
+
+    ${renderLicenseLink(data.license)}
+
+
+    * [Test](#test)
+
+
+    # Dependencies
+
+
+    To make sure you are able to run this application please download the dependencies with the following command;
+
+    ===============================================
+    ${data.dependencies}
+    ===============================================
+
+    # Uses
+
+
+    ${data.usage}
+
+
+
+    ${renderLicenseSection(data.license)}
+
+
+    # Email
+
+
+    You can reach me at: ${data.emailAdress}
+
+
+
+    # Testing
+
+
+    For tests, use the following command;
+
+    ========================================
+    ${data.test}
+    ========================================
+    
 `;
 }
 
---
+++ fileDump/README.md
@@ -0,0 +1,73 @@
+# Fingerprint app animation
+
+
+    ![GitHub license](https://img.shields.io/badge/license-MIT-red.svg)
+
+
+    # GitHub Link
+
+
+    [CalebLuster](https://github.com/CalebLuster/)
+
+
+    # Project Description
+
+
+    A simple animation using only HTML and CSS!
+
+
+    # Table of Contents
+
+
+    * [Uses](#usage)
+
+
+    * [Installations](#dependencies)
+
+
+    
+* [License](#license)
+
+
+
+    * [Test](#test)
+
+
+    # Dependencies
+
+
+    To make sure you are able to run this application please download the dependencies with the following command;
+
+    ===============================================
+    npm 1
+    ===============================================
+
+    # Uses
+
+
+    Easy Cool Phone Quick
+
+
+
+    ### License 
+      Licensed under MIT.
+      Copyright MIT. All rights reserved.
+      
+
+
+    # Email
+
+
+    You can reach me at: cwl117@outlook.com
+
+
+
+    # Testing
+
+
+    For tests, use the following command;
+
+    ========================================
+    npm test
+    ========================================
+    
